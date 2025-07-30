d30270cb18d0fad8cdc142b92e8f88f446f5705e
Fabricio3916 <fabriciopassos3916@gmail.com>
2025-07-30 15:33:20 -0300

    build: setup inicial do projeto e configuração do application.properties

---
+++ src/main/java/dev/cadastrodecomputador/CadastroDeComputadorApplication.java
@@ -0,0 +1,13 @@
+package dev.cadastrodecomputador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeComputadorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadastroDeComputadorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/cadastrodecomputador/CadastroDeComputadorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cadastrodecomputador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeComputadorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
